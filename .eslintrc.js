552851db94ff6b0fa07165e9bc63d6ebd270afcc
Santiago Tamayo <stamayol@unal.edu.co>
2022-04-20 14:28:05 -0500

    “setup cypress configuration completed”

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    plugins: [
+        '@typescript-eslint',
+        "cypress"
+    ],
+    "rules": {
+        "quotes": ["error", "double"],
+        "cypress/no-assigning-return-values": "error",
+        "cypress/no-unnecessary-waiting": "error",
+        "cypress/assertion-before-screenshot": "warn",
+        "cypress/no-force": "warn"
+    },
+    "env": {
+        "cypress/globals": true
+    }
+}
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,18 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+/// <reference types="cypress" />
+
+const wp = require('@cypress/webpack-preprocessor')
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+    const options = {
+        webpackOptions: require('../../webpack.config'),
+    }
+
+    on('file:preprocessor', wp(options))
+}
